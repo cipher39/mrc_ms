aba59ab1dbc2c925ca589889f6c9a9bb8015b4aa
Santosh Sahni <meet.santosh96@gmail.com>
2026-03-27 22:43:40 +0530

    MRC Roadways

---
+++ MRC Roadways/MRC-Roadways/src/main/java/com/mrc/roadways/MrcRoadwaysApplication.java
@@ -0,0 +1,13 @@
+package com.mrc.roadways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrcRoadwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrcRoadwaysApplication.class, args);
+	}
+
+}
---
+++ MRC Roadways/MRC-Roadways/src/test/java/com/mrc/roadways/MrcRoadwaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrc.roadways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrcRoadwaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
